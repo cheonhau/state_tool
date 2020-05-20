4561269c2a236b1e076fd028aa4e0217b67197b5
mrjohuy <huynh@onetech.vn>
2020-05-20 22:26:10 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 // document : https://medium.com/flutter-community/making-sense-all-of-those-flutter-providers-e842e18f45dd
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
+import './providers/products.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,17 +14,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'My Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName : (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName : (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
